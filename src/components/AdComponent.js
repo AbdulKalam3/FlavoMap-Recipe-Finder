a65593830509753214ad43771124c6115168ae6e
AbdulKalam3 <abdul.kalam03123@gmail.com>
2025-01-02 14:16:13 +0530

    Integrated Google AdSense

--- src/components/AdComponent.js
+++ src/components/AdComponent.js
@@ -8,9 +8,6 @@ function AdComponent() {
            data-ad-client="pub-7360554667394701"
            data-ad-slot="6198673929"
            data-ad-format="auto"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
     </div>
   );
 }
