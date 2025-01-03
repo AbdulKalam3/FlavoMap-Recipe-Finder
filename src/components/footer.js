bcb9e00dbf1cfec0e191f44577a1668f9338a049
AbdulKalam3 <abdul.kalam03123@gmail.com>
2025-01-03 11:18:01 +0530

    Added Google AdSense ad component

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,18 +2,19 @@ import './css/Footer.css';
 
 function Footer(){
     return(
-        <footer>{/* Google AdSense Ad */}
-        <div className="ads-container">
-          <ins className="adsbygoogle"
-            style={{ display: 'block', textAlign: 'center' }}
-            data-ad-client="ca-pub-7360554667394701"
-            data-ad-slot="1234567890"
-            data-ad-format="auto">
-          </ins>
-        </div>
+        <footer>
+            {/* Google AdSense Ad */}
+            <div className="ads-container">
+                <ins className="adsbygoogle"
+                    style={{ display: 'block', textAlign: 'center' }}
+                    data-ad-client="ca-pub-7360554667394701"
+                    data-ad-slot="1234567890"
+                    data-ad-format="auto">
+                </ins>
+            </div>
             <div className='footer'>
                 <p>
-                © 2025 Deliciously coded by Shaik Abdul Kalam – Bringing recipes to your fingertips!
+                    © 2025 Deliciously coded by Shaik Abdul Kalam – Bringing recipes to your fingertips!
                 </p>
             </div>
         </footer>
