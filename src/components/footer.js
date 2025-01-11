5c4b10c7446cfbdc05ad1db40a481afd83c1e865
AbdulKalam3 <abdul.kalam03123@gmail.com>
2025-01-11 10:52:50 +0530

    Google Adsense update

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,15 +4,6 @@ import React from 'react';
 function Footer(){
     return(
         <section>
-            {/* Google AdSense Ad */}
-            <div className="ads-container">
-                <ins className="adsbygoogle"
-                    style={{ display: 'block', textAlign: 'center' }}
-                    data-ad-client="ca-pub-7360554667394701"
-                    data-ad-slot="1234567890"
-                    data-ad-format="auto">
-                </ins>
-            </div>
         <footer>
             <div className='footer1'>
                 <p>
--- src/components/main.js
+++ src/components/main.js
@@ -133,6 +133,18 @@ function Main() {
           )}
         </div>
       </div>
+
+      <div className='row'>
+        {/* Google AdSense Ad */}
+        <div className="ads-container">
+                <ins className="adsbygoogle"
+                    style={{ display: 'block', textAlign: 'center' }}
+                    data-ad-client="ca-pub-7360554667394701"
+                    data-ad-slot="1234567890"
+                    data-ad-format="auto">
+                </ins>
+            </div>
+      </div>
     </div>
   );
 }
